48ad5657aa264b187e8a171fdba7436b4446ef16
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2023-02-02 00:24:49 +0900

    Add : 기본 설정

---
+++ SBR_Unity/Assets/Script/Service/GameLogger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SBR
+{
+    public interface IGameLogger
+    {
+
+    }
+    public class GameLogger
+    {
+
+    }
+}
